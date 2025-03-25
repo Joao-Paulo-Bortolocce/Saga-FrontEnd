0fd246135b0ae7fcf9d98c144d74abfcba396b81
Joao-Paulo-Bortolocce <bbortolocce@gmail.com>
2025-03-25 13:10:47 -0300

    personalisando

--- src/components/pages/HomeCadastros.jsx
+++ src/components/pages/HomeCadastros.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import Page from "../layouts/Page";
+import { BookUser, Backpack, User ,Shapes} from "lucide-react";
 
-const todosTipos = ["Pessoas", "Infra", "Academico"];
 
 const cadastros = [
-    { nome: "Cadastro de Aluno", href: "/cadastros/aluno", tipo: "Pessoas" },
-    { nome: "Cadastro de Profissional", href: "/cadastros/profissional", tipo: "Pessoas" },
-    { nome: "Cadastro de Responsavel", href: "/cadastros/pessoa", tipo: "Pessoas" },
-    { nome: "Cadastro de Serie", href: "/cadastros/serie", tipo: "Infra" }
+    { nome: "Cadastro de Aluno", href: "/cadastros/aluno", tipo: "Pessoas" ,icon:Backpack},
+    { nome: "Cadastro de Profissional", href: "/cadastros/profissional", tipo: "Pessoas" , icon:BookUser},
+    { nome: "Cadastro de Responsável", href: "/cadastros/pessoa", tipo: "Pessoas", icon:User},
+    { nome: "Cadastro de Serie", href: "/cadastros/serie", tipo: "Infra" , icon:Shapes}
 ]
 
 export default function HomeCadastros(props) {
@@ -44,41 +44,39 @@ export default function HomeCadastros(props) {
     return (
         <>
             <Page />
-            <main className="flex flex-col ">
-                <section className="m-20 justify-evenly flex row">
-                    <div className="w-3/6"><input style={{width:"100%"}} className="border-4 border-solid rounded-xl pl-4" placeholder="    Digite um filtro" type="text" id="filtroInput" value={filtro} onChange={manipularInput} /></div>
-                    <div className="w-2/6 ">
-                        <select className="border-4 border-solid rounded-xl" name="tipoSelect" id="tipoSelect" value={tipo} onChange={manipularSelect}>
-                            <option value="todos">Todos</option>
-                            <option value="Pessoas">Pessoas</option>
-                            <option value="Infra">Infraestrutura</option>
-                            <option value="Academico">Acadêmico</option>
-                        </select>
-                    </div>
-                </section>
-                <section className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 m-10">
-                    {
-                        cadastros.map((cadastro) => {
-                            if (cadastro.nome.toLowerCase().includes(filtro.toLowerCase())) {
-                                if (tipo == "todos" || cadastro.tipo === tipo)
-                                    return (
-                                        <div className="group relative border-4 border-double rounded-xl p-4 hover:bg-sky-700 ">
-                                            <div className="mt-4 flex justify-between ">
-                                                <div>
-                                                    <h3 className="text-sm text-gray-700 hover:text-white">
-                                                        <a href={cadastro.href}>
-                                                            <span aria-hidden="true" className="absolute inset-0" />
-                                                            {cadastro.nome}
-                                                        </a>
-                                                    </h3>
-                                                    <p className="mt-1 text-sm text-gray-500 hover:text-white">Tipo: {cadastro.tipo}</p>
+            <main >
+                <section style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <section className=" justify-evenly flex row" style={{ width: "75%" }}>
+                        <div className="w-3/5"><input style={{ width: "100%" }} className="border-4 border-solid rounded-xl pl-4" placeholder="    Digite um filtro" type="text" id="filtroInput" value={filtro} onChange={manipularInput} /></div>
+                        <div className="w-1/5 ">
+                            <select className="border-4 border-solid rounded-xl" name="tipoSelect" id="tipoSelect" value={tipo} onChange={manipularSelect}>
+                                <option value="todos">Todos</option>
+                                <option value="Pessoas">Pessoas</option>
+                                <option value="Infra">Infraestrutura</option>
+                                <option value="Academico">Acadêmico</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 " style={{ width: "75%" }}>
+                        {
+                            cadastros.map((cadastro) => {
+                                if (cadastro.nome.toLowerCase().includes(filtro.toLowerCase())) {
+                                    if (tipo == "todos" || cadastro.tipo === tipo)
+                                        return (
+                                            <div className="transform hover:scale-105 transition-transform bg-white rounded-xl shadow-lg overflow-hidden">
+                                                <div className="bg-orange-500 h-2"></div>
+                                                <div className="p-6">
+                                                    <cadastro.icon className="w-12 h-12 mx-auto mb-4 text-orange-500" />
+                                                    <p className="text-center font-semibold text-gray-800 text-lg">
+                                                        {cadastro.nome}
+                                                    </p>
                                                 </div>
                                             </div>
-                                        </div>
-                                    );
-                            }
-                        })
-                    }
+                                        );
+                                }
+                            })
+                        }
+                    </section>
                 </section>
             </main>
         </>
