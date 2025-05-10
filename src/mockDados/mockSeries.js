8d4cad186f80953f53eb8534a559c376fffc6215
Joao-Paulo-Bortolocce <bbortolocce@gmail.com>
2025-05-10 14:28:45 -0300

    limpando a main

--- src/mockDados/mockSeries.js
+++
@@ -1,8 +0,0 @@
-export const listaDeSeries=[
-    { "serie_id": 1, "serie_num": 1, "serie_descr": "1º ano" },
-    { "serie_id": 2, "serie_num": 2, "serie_descr": "2º ano" },
-    { "serie_id": 3, "serie_num": 3, "serie_descr": "3º ano" },
-    { "serie_id": 4, "serie_num": 4, "serie_descr": "4º ano" },
-    { "serie_id": 5, "serie_num": 5, "serie_descr": "5º ano" }
-  ]
-  
